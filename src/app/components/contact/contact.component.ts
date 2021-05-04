af42aefa44d0868820592bbd62a458f655d94bab
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-05-04 10:16:42 -0600

    Added some links on the contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  sites = [
+    { name: 'LinkedIn', username: 'Sam Leishman', link: 'https://www.linkedin.com/in/sam-leishman-9201471b5/', logo: '../../assets/linkedInLogo.png', hover: '../../assets/linkedInLogoColor.png', src: '' },
+    { name: 'Email', username: 'samuelleish@gmail.com', link: 'mailto: samuelleish@gmail.com', logo: '../../assets/emailLogo.png', hover: '../../assets/gmailLogo.png', src: '' },
+    { name: 'Instagram', username: '@sam__leishman', link: 'https://www.instagram.com/sam__leishman/', logo: '../../assets/instagramLogo.png', hover: '../../assets/altInstagramLogo.png', src: '' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
