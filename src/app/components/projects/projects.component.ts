1f641c0ee02c1bb258b7d2c2275976e0645f02c6
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-06-17 12:17:15 -0600

    Fix some styling on projects page to make it more responsive

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,21 +9,19 @@ export class ProjectsComponent implements OnInit {
 
   javascriptProjects = [
     { name: 'To-Do App', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/toDoApp/' },
-    { name: 'Pokémon', description: 'This was my first project where I worked with an API. I used an API that had all sorts of data on Pokémon', image: 'url', url: 'https://sam-leishman.github.io/pokemonApp/' },
+    { name: 'Pokémon', description: 'This was my first project where I worked with an API. I used an API that had all sorts of data on Pokémon and used it to display them by region.', image: 'url', url: 'https://sam-leishman.github.io/pokemonApp/' },
     { name: 'Golf Scorecard', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/golfScoreCardApp/' }
   ];
   backendProjects = [
     { name: 'blah', description: 'blah blah blah', image: 'url', url: 'url' }
   ];
+  otherProjects = [
+    { name: 'RYParis', description: 'A Mock-Up project designed in basic HTML/CSS (no JavaScript).', image: 'url', url: 'https://sam-leishman.github.io/ryParis/' }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  goToLink(url: string) {
-    window.open(url, "_blank");
-    // use <a (click)="goToLink("www.example.com")">page link</a> in html
   }
 
 }
