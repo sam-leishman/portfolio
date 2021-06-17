3ab73f48599b2ea289e64d367fb4701660b341bf
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-06-16 19:39:54 -0600

    Added GitHub profile to contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -10,7 +10,8 @@ export class ContactComponent implements OnInit {
   sites = [
     { name: 'LinkedIn', username: 'Sam Leishman', link: 'https://www.linkedin.com/in/sam-leishman-9201471b5/', logo: '../../assets/linkedInLogo.png', hover: '../../assets/linkedInLogoColor.png', src: '' },
     { name: 'Email', username: 'samuelleish@gmail.com', link: 'mailto: samuelleish@gmail.com', logo: '../../assets/emailLogo.png', hover: '../../assets/gmailLogo.png', src: '' },
-    { name: 'Instagram', username: '@sam__leishman', link: 'https://www.instagram.com/sam__leishman/', logo: '../../assets/instagramLogo.png', hover: '../../assets/altInstagramLogo.png', src: '' }
+    { name: 'Instagram', username: '@sam__leishman', link: 'https://www.instagram.com/sam__leishman/', logo: '../../assets/instagramLogo.png', hover: '../../assets/altInstagramLogo.png', src: '' },
+    { name: 'GitHub', username: 'sam-leishman', link: 'https://github.com/sam-leishman', logo: '../../assets/gitHubLogo.png', hover: '../../assets/altGitHubLogo.png', src: '' }
   ];
 
   constructor() { }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  javascriptProjects = [
+    { name: 'To-Do App', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/toDoApp/' },
+    { name: 'Pokémon', description: 'This was my first project where I worked with an API. I used an API that had all sorts of data on Pokémon', image: 'url', url: 'https://sam-leishman.github.io/pokemonApp/' },
+    { name: 'Golf Scorecard', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/golfScoreCardApp/' }
+  ];
+  backendProjects = [
+    { name: 'blah', description: 'blah blah blah', image: 'url', url: 'url' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  goToLink(url: string) {
+    window.open(url, "_blank");
+    // use <a (click)="goToLink("www.example.com")">page link</a> in html
   }
 
 }
