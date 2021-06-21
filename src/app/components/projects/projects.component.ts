37d3efe72b1bc8db7d2dd749e2e848f79467b3ec
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-06-20 20:38:15 -0600

    Get rid of project categories and combine into one array

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,16 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  javascriptProjects = [
-    { name: 'To-Do App', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/toDoApp/' },
-    { name: 'Pokémon', description: 'This was my first project where I worked with an API. I used an API that had all sorts of data on Pokémon and used it to display them by region.', image: 'url', url: 'https://sam-leishman.github.io/pokemonApp/' },
-    { name: 'Golf Scorecard', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/golfScoreCardApp/' }
-  ];
-  backendProjects = [
-    { name: 'blah', description: 'blah blah blah', image: 'url', url: 'url' }
-  ];
-  otherProjects = [
-    { name: 'RYParis', description: 'A Mock-Up project designed in basic HTML/CSS (no JavaScript).', image: 'url', url: 'https://sam-leishman.github.io/ryParis/' }
+  projects = [
+    { name: 'RYParis', developed: 'HTML/CSS', description: 'A Mock-Up project designed in basic HTML/CSS (no JavaScript).', image: 'url', url: 'https://sam-leishman.github.io/ryParis/' },
+    { name: 'Nick Cage', developed: 'HTML/CSS', description: 'I don\'t really have much to say about this one. You might regret clicking on it. (no JavaScript).', image: 'url', url: 'https://sam-leishman.github.io/bootstrapCarousel/' },
+    { name: 'To-Do App', developed: 'JavaScript', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/toDoApp/' },
+    { name: 'Pokémon', developed: 'JavaScript', description: 'This was my first project where I worked with an API. I used an API that had all sorts of data on Pokémon and used it to display them by region.', image: 'url', url: 'https://sam-leishman.github.io/pokemonApp/' },
+    { name: 'Golf Scorecard', developed: 'JavaScript', description: 'blah blah blah', image: 'url', url: 'https://sam-leishman.github.io/golfScoreCardApp/' },
+    { name: 'Spiffy Chat', developed: 'React', description: 'This was my first collaborative project. We built this chat app in React.', image: 'url', url: 'https://sam-leishman.github.io/chat-app/' }
   ]
 
   constructor() { }
